f0ac1a0e9c76d04869a4593ee481e55a8d078cf7
Prabhakar <wassupprabhakar@gmail.com>
2023-01-06 10:14:53 +0530

    filter on trainer dashboard

--- pages/components/trainerDashboardCourseList.js
+++ pages/components/trainerDashboardCourseList.js
@@ -14,7 +14,7 @@ export default function adminDashboardGraph() {
   QueryParam.page = router.query.page || 1;
   QueryParam.order_by = router.query?.order_by || "created_at";
   QueryParam.order_in = router.query?.order_in || "desc";
-  const { data: courses, error: courseerror, isLoading: courseisLoading } = useSWR(QueryParam ? "courseList" : null, async () => await courseModel.list(QueryParam), config.swrConfig);
+  const { data: courses, error: courseerror, isLoading: courseisLoading, mutate:loadCourse } = useSWR(QueryParam ? "courseList" : null, async () => await courseModel.list(QueryParam), config.swrConfig);
   const { data: categoryData, error: categoryerror, isLoading: categoryisLoading } = useSWR(QueryParam ? "categorylist" : null, async () => await categoryModel.list(), config.swrConfig);
   const pagginationHandler = (page) => {
     QueryParam.page = page.selected + 1;
@@ -23,13 +23,23 @@ export default function adminDashboardGraph() {
       query: QueryParam,
     });
   };
+
+  const changeCategory = (value) => {
+    //console.log(value);
+    QueryParam.category_id = value;
+    router.push({
+      pathname: router.pathname,
+      query: QueryParam,
+    });
+    loadCourse();
+  }
   return (
     <>
       <div class="category-create-btn d-flex justify-content-between">
 
         <div class="category d-flex gap-3 align-items-center">
           <h6 for="category" style={{ color: "#7E878C", fontFamily: "Co-text" }}>Category: </h6>
-          <select name="category" id="cars" className='select-dashboard'>
+          <select name="category" id="cars" className='select-dashboard' onChange={() => { changeCategory(event.target.value) }}>
             <option value="Country">-Select-</option>
             {categoryData?.data?.map((item) => {
               return (<option key={item.id} value={item.id}>{item.category_name}</option>)
