66e97aef8acca5c3d1a2e605bcd04c25060e616c
Prabhakar <wassupprabhakar@gmail.com>
2023-01-09 19:29:49 +0530

    edit update

--- pages/courses/[id]/edit.js
+++ pages/courses/[id]/edit.js
@@ -111,7 +111,7 @@ const editCourse = () => {
                                 </div>
                                 <div className="launch-date">
                                     <h6>Course Launch Date</h6>
-                                    <input className="min-date" type="date" {...register("course_launch_date")}  max={moment().format("YYYY-MM-DD")}  />
+                                    <input className="min-date" type="date" {...register("course_launch_date")}  min={moment().format("YYYY-MM-DD")}  />
                                 </div>
                             </div>
 
