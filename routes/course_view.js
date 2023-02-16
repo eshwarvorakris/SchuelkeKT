5f44cd2eb79ca43b353bb45fa53d5a0ddec7e007
Prabhakar <wassupprabhakar@gmail.com>
2023-02-16 18:13:09 +0530

    user verify

--- routes/course_view.js
+++ routes/course_view.js
@@ -4,12 +4,12 @@ const courceViewController = require("../controllers/courceViewController");
 // middleware that is specific to this router
 const verifyAuth = require("../middleware/authorization");
 
-router.post("/get_any_course_chapter_viewed", verifyAuth.verifyTrainee, courceViewController.getAnyCourseChapterViewed);
-router.post("/get_chapter_view", verifyAuth.verifyTrainee, courceViewController.getChapterView);
-router.post("/get_module_view", verifyAuth.verifyTrainee, courceViewController.getModuleView);
+router.post("/get_any_course_chapter_viewed", verifyAuth.verifyUser, courceViewController.getAnyCourseChapterViewed);
+router.post("/get_chapter_view", verifyAuth.verifyUser, courceViewController.getChapterView);
+router.post("/get_module_view", verifyAuth.verifyUser, courceViewController.getModuleView);
 router.get("/", courceViewController.index);
 router.get("/:id", courceViewController.show);
-router.post("/", verifyAuth.verifyTrainee, courceViewController.store);
+router.post("/", verifyAuth.verifyUser, courceViewController.store);
 router.put("/:id", courceViewController.update);
 router.delete("/", courceViewController.destroy);
 
